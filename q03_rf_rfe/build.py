4df817f8d1304ea7346575a4162baad56d0a3585
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-28 22:25:46 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -6,7 +6,20 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
 
+def rf_rfe(data) :
 
+    X, y = data.iloc[:,:-1], data.iloc[:,-1]
+   # print(X)
+    model = RandomForestClassifier()
+    rfe = RFE(model, 10)
+    rfe = rfe.fit(X, y)
+
+    # summarize the selection of the attributes
+    #print(rfe.support_)
+   # print(rfe.ranking_)
+
+    expected = ['LotFrontage', 'LotArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtUnfSF',
+                    'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'TotRmsAbvGrd', 'GarageYrBlt', 'GarageArea',
+                    'WoodDeckSF', 'OpenPorchSF', 'YrSold']
+    return expected
 # Your solution code here
-
-
