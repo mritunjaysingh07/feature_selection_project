1e4d6adf5b5fb810eb4587724a6902a4ecb387c7
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-28 21:46:24 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,8 +1,13 @@
 # Default imports
 import pandas as pd
 from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
+def plot_corr(data,size = 11):
+    plt.figure(figsize=(10,6))
+    sns.heatmap(data.corr())
 
 
 # Write your solution here:
