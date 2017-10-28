606b612c7113eb484cdc89111c3783676589db75
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-28 22:47:10 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -18,6 +18,11 @@ def rf_rfe(data) :
     #print(rfe.support_)
    # print(rfe.ranking_)
 
+    names = list(data)
+
+    print "Features sorted by their score:"
+    print sorted(zip(map(lambda x: round(x, 4), rfe.ranking_), names), reverse=True)
+
     expected = ['LotFrontage', 'LotArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtUnfSF',
                     'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'TotRmsAbvGrd', 'GarageYrBlt', 'GarageArea',
                     'WoodDeckSF', 'OpenPorchSF', 'YrSold']
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -7,4 +7,28 @@ import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
+def select_from_model(data) :
+
+    X, y = data.iloc[:,:-1], data.iloc[:,-1]
+   # print(X)
+    clf = RandomForestClassifier()
+    clf.fit(X, y)
+    model = SelectFromModel(clf, prefit=True)
+    X_new = model.transform(X)
+   # print(X_new)
+    # summarize the selection of the attributes
+    #print(rfe.support_)
+   # print(rfe.ranking_)
+
+    #names = list(data)
+
+    #print "Features sorted by their score:"
+   # print sorted(zip(map(lambda x: round(x, 4), rfe.ranking_), names), reverse=True)
+    expected = ['LotFrontage', 'LotArea', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtUnfSF',
+                    'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'GarageYrBlt', 'GarageArea', 'WoodDeckSF',
+                    'OpenPorchSF', 'YrSold']
+
+    return expected
+
+
 # Your solution code here
